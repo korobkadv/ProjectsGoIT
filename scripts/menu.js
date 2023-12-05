862e7fd10625fa5e564489e873ef0bffb5e72e8e
korobkadv <korobka_dv@ukr.net>
2023-12-05 18:47:37 +0200

    bugs fixed

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,18 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-  menuBtnRef.addEventListener("click", () => {
+  const lowerBlock = document.querySelector('.body');
+
+  menuBtnRef.addEventListener('click', () => {
+    lowerBlock.classList.toggle('overflow-hidden');
+
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle('is-open');
   });
 })();
