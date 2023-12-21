2faaca9893decedcdac0840a105b48b6188b87b5
korobkadv <korobka_dv@ukr.net>
2023-12-21 18:16:05 +0200

    add home work 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const cardsGallery = createCardGallery(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+galleryContainer.addEventListener("click", openModalImages);
+
+// Створюємо галерею
+function createCardGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
+// Створюємо зображення для модального вікна
+function createModalImages(img) {
+  return basicLightbox.create(`<img src="${img}" width="1280" height="800">`);
+}
+
+// Відкриваємо модальне вікно з зображенням
+function openModalImages(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const image = createModalImages(evt.target.dataset.source);
+
+  image.show();
+
+  // Закриття модального вікна по Escape
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      image.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const cardsGallery = createCardGallery(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+new SimpleLightbox(".gallery__link", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+// Створюємо галерею
+function createCardGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>
+        `;
+    })
+    .join("");
+}
