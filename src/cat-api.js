5dca5ca0ecaf2ef5be33b5fbaff982879d1f89d2
korobkadv <korobka_dv@ukr.net>
2024-01-05 01:38:04 +0200

    add Slim Select

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,60 +6,22 @@
 
 const API_KEY =
   'live_JgB0clPuL3nvKc4JFyvm6ISJhaKn3bvHlq3E5wx3cZ8JOdFI2w35Nnjv6MEpLfrB';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
-// Знаходимо необхідну HTML
-const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-
-// Запит для отримання назв котів
+// Запит для отримання назв породи котів
 function fetchBreeds() {
-  return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`).then(
-    resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
     }
-  );
-}
-
-// Обробляємо запит для назв породи котів
-fetchBreeds()
-  .then(data => {
-    breedSelect.insertAdjacentHTML('beforeend', createMarkup(data));
-  })
-  .catch(err => console.log(err));
-
-function createMarkup(arr) {
-  return arr
-    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-    .join('');
-}
-
-// Обробляєм вибір породи кота
-breedSelect.addEventListener('change', event => {
-  fetchCatByBreed(event.currentTarget.value)
-    .then(data => {
-      catInfo.textContent = '';
-      catInfo.insertAdjacentHTML('beforeend', createMarkupCatCard(data));
-    })
-    .catch(err => console.log(err));
-});
-
-// Малюємо карточку однієї породи кота
-function createMarkupCatCard(arr) {
-  return `
-  <img src='${arr[0].url}' width='350' align='left' alt='${arr[0].breeds[0].name}'>
-  <h2>${arr[0].breeds[0].name}</h2>
-  <p>${arr[0].breeds[0].description}</p>
-  <p><b>Temperament: </b>${arr[0].breeds[0].temperament}</p>
-    `;
+    return resp.json();
+  });
 }
 
 // Запит на одну породу кота
 function fetchCatByBreed(breedId) {
   return fetch(
-    `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
   ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
@@ -69,4 +31,4 @@ function fetchCatByBreed(breedId) {
 }
 
 // Експортуємо функції по назві
-export { breedSelect, fetchBreeds };
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-import * as catApi from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-catApi.fetchBreeds();
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+// Знаходимо необхідну HTML
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+loader.hidden = true;
+error.hidden = true;
+
+// Обробляємо запит для назв породи котів
+fetchBreeds()
+  .then(data => {
+    breedSelect.insertAdjacentHTML('beforeend', createMarkup(data));
+    new SlimSelect({
+      select: '#single',
+    });
+  })
+  .catch(err => {
+    error.hidden = false;
+    console.log(err);
+  });
+
+function createMarkup(arr) {
+  return arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+}
+
+// Обробляєм вибір породи кота
+breedSelect.addEventListener('change', event => {
+  catInfo.textContent = '';
+  loader.hidden = false;
+
+  fetchCatByBreed(event.currentTarget.value)
+    .then(data => {
+      catInfo.insertAdjacentHTML('beforeend', createMarkupCatCard(data));
+      loader.hidden = true;
+    })
+    .catch(err => {
+      loader.hidden = true;
+      error.hidden = false;
+      console.log(err);
+    });
+});
+
+// Малюємо карточку однієї породи кота
+function createMarkupCatCard(arr) {
+  return `
+  <img src='${arr[0].url}' width='350' align='left' alt='${arr[0].breeds[0].name}'>
+  <h2>${arr[0].breeds[0].name}</h2>
+  <p>${arr[0].breeds[0].description}</p>
+  <p><b>Temperament: </b>${arr[0].breeds[0].temperament}</p>
+    `;
+}
