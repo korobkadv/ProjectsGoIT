2bed3e765922398cdc350c3e3d7ef1be2d0939c2
korobkadv <korobka_dv@ukr.net>
2024-01-25 02:14:48 +0000

    Deploying to goit-react-hw-03-image-finder from @ korobkadv/ProjectsGoIT@dd3acd0068f760d6de5abbefaa438f77ceb60df7 🚀

--- src/components/Button/Button.js
+++
@@ -1,5 +0,0 @@
-import { Submit } from './Button.styled';
-
-export const Button = ({ title, loadMore }) => {
-  return <Submit onClick={loadMore}>{title}</Submit>;
-};
--- src/components/Modal/Modal.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ModalImage = styled.img`
-  max-width: calc(100vw - 48px);
-  max-height: calc(80vh - 24px);
-`;
