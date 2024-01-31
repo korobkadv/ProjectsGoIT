26b7b43181539fbe2b893fe87f9f207fca0e7c78
korobkadv <korobka_dv@ukr.net>
2024-01-31 16:19:57 +0000

    Deploying to gh-pages-react-hw-05-movies from @ korobkadv/ProjectsGoIT@61b1d416d348ca952f88857724228ce4166971a4 🚀

---
+++ static/js/791.7fb5c715.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{680:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(87),s=t(184);function u(){return(0,s.jsxs)("div",{children:["Sorry! Error! Please use this ",(0,s.jsx)(n.rU,{to:"/",children:"link"})]})}}}]);
+//# sourceMappingURL=791.7fb5c715.chunk.js.map
